c8ac523106be576c6a4cf9eca9ac19d236f59ce6
Klaus Wolfgram <klaus.wolfgram@gmail.com>
2026-04-23 21:12:30 -0300

    toolbar e networkservice

--- src/app/core/services/network-service.ts
+++ src/app/core/services/network-service.ts
@@ -1,8 +1,39 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable, NgZone } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class NetworkService {
+
+  private onLineApp = new BehaviorSubject<boolean>(navigator.onLine);
+  private zone = inject(NgZone);
+
+  public onLine$ = this.onLineApp.asObservable();
+
+  constructor() {
+    this.startMonitoring();
+  }
+
+  private startMonitoring = () => {
+    window.addEventListener('online' , () => this.zone.run(() => this.onLineApp.next(true )));
+    window.addEventListener('offline', () => this.zone.run(() => this.onLineApp.next(false)));
+  }
+
+  public isOnline = (): boolean => this.onLineApp.value; 
+
+  public checkInternet = async () => {
+    
+    if(!navigator.onLine){
+      return false;
+    }
+
+    try {
+      const response = await fetch('https://www.google.com/generate_204',{method: 'HEAD', cache: 'no-cache'});
+      return response.ok;
+    } catch (e) {
+      return false;
+    }
+  }
   
 }
--- src/app/shared/toolbar/toolbar.ts
+++ src/app/shared/toolbar/toolbar.ts
@@ -1,14 +1,33 @@
-import { Component, inject } from '@angular/core';
+import { Component, HostListener, inject, signal } from '@angular/core';
 import { ToolbarService } from '../../core/services/toolbar-service';
+import { CommonModule } from '@angular/common';
+import { NetworkService } from '../../core/services/network-service';
+import { AuthService } from '../../core/services/auth-service';
+import { environment } from '../../core/environments/environment';
 
 @Component({
   selector: 'app-toolbar',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './toolbar.html',
   styleUrl: './toolbar.scss',
 })
 export class Toolbar {
 
   public toolbarService = inject(ToolbarService);
+  public networkService = inject(NetworkService);
+  public authService = inject(AuthService);
+  public version = signal<string>(environment.versao);
+
+  public showProfileMenu = signal<boolean>(false);
+
+  public toggleProfileMenu = (event: MouseEvent) => {
+    event.stopPropagation();
+    this.showProfileMenu.update(v => !v);
+  }
+
+  @HostListener('document:click',['$event'])
+  public onCloseMenu = (event: MouseEvent) => {
+    if(this.showProfileMenu()) this.showProfileMenu.set(false);
+  }
 
 }
