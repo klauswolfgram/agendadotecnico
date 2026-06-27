dc8231566037bbe545a142cd3d1a57ecc16d319c
Klaus Wolfgram <klaus.wolfgram@gmail.com>
2026-06-27 13:18:59 -0300

    feat: atualizar versão para 1.0 e adicionar splash screen ao aplicativo

--- src/app/core/environments/environment.development.ts
+++ src/app/core/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
     ambiente: 'dev',
-    versao: '0.6 - 06/05/2026',
+    versao: '1.0',
     url_base: 'https://erp:2200/api/',
     tenantId: '99,01',
     STORAGE_NAME: 'agenda-do-tecnico',
